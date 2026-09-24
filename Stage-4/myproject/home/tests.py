388398806aa06dd2783e8d6ce740221469c2fa20
Dhara Patel <dhara6744@gmail.com>
2026-09-24 18:54:33 +0530

    Learned about test cases

--- Stage-4/myproject/home/tests.py
+++ Stage-4/myproject/home/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        response = self.client.get('/about/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page(self):
+        response = self.client.get('/contact/')
+
+        self.assertEqual(response.status_code, 200)
